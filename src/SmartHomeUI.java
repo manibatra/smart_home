833408648b6fd249810f047ec266cec568c0e74c
Mani Batra <manibatra2002@gmail.com>
2015-04-20 22:01:22 +1000

    Added new files

---
+++ src/SmartHomeUI.java
@@ -0,0 +1,25 @@
+class SmartHomeUI extends Ice.Application{
+
+	public static void main(String args[]){
+		
+		System.out.println("Welcome to the Smart Home Monitoring System\n"
+				+ "Please select an option:\n"
+				+ "1. View log – temperature adjustment\n"
+				+ "2. View media files\n"
+				+ "3. View disc tracks\n"
+				+ "E. Exit");
+		
+		
+	}
+	
+	@Override
+	public int run(String[] args) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	
+	
+	
+}
